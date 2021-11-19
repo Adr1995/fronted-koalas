452c9f487170596f485226589d00b440a4806f72
Adrian Arbelo <adri.arb.hdez@gmail.com>
2021-11-19 21:13:59 +0000

    [FTR] Login implementado (falta probarlo) y comienzo del Signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login-component/login.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login-component/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
+export class LoginComponent implements OnInit{
+//   email!: string;
+//   password!: string;
+
+//   constructor(public userService: UserService) {}
+
+//   ngOnInit(): void {
+//   }
+
+//   login() {
+//     const user = {email: this.email, password: this.password};
+//     this.userService.login(user).subscribe( data => {
+//       console.log(data);
+//     });
+//   }
+// }
+
+loginForm = { email : "yunior+eoi5@squaads.com", password : "990011"}
+
+
+constructor( 
+  private authService : AuthService,
+  private router : Router,
+  private toastr: ToastrService
+  ) { }
+
+ngOnInit(): void {
+}
+
+login(){
+  this.authService.login( this.loginForm )
+    .then(res => {
+      debugger;
+      localStorage.setItem('token', res.token); // esto se hace una vez que ha llegado el token, y lo que hace es guardarlo en el navegador
+      // console.log(res) //para ver el token, esto no hay que dejarlo, es solo para verlo
+      // this.logged = true;
+      this.router.navigateByUrl('/')
+    })
+    .catch(err => {
+      if (err.status == 400){
+      this.toastr.error("No se ha podido acceder a su usuario", "Error!")
+    } else if(err.status = 500) {
+      this.toastr.error("Internal server error", "Error 500")
+      }
+      // if (err.status == 400){
+      //   this.toastr.error(err.data.message, 'Error')
+      // }
+    })
+}
+
+}
